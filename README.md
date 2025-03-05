571a8ded8ad6515e99cdd2e4fd3040b3edde08f6
AbdulAzizRohan <dev.abdulazizrohan@gmail.com>
2025-03-05 21:47:39 +0600

    finished

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a required certification project for freeCodeCamp JavaScript and Algorithms and Data Structures course.
+
+The CSS for this project has been generated using DeepSeek.
+
+**Roman Numeral Converter**
+
+- Give a decimal number within the range of 1 and 3999.
+- Get the equivalent Roman Numeral.
---
+++ script.js
@@ -0,0 +1,105 @@
+const convertBtn = document.getElementById("convert-btn");
+const number = document.getElementById("number");
+const output = document.getElementById("output");
+
+const romanNumerals = ["I", "V", "X", "L", "C", "D", "M"];
+
+const convertRomanNumeral = (number) => {
+  number = number.toString();
+  number = number.split("").reverse().join("");
+
+  let ans = "";
+
+  for (let i = 0; i < number.length; i++) {
+    const n = parseInt(number[i]);
+
+    if (n === 0) continue;
+
+    switch (n) {
+      case 1:
+        ans = romanNumerals[2 * i] + ans;
+        break;
+
+      case 2:
+        ans = romanNumerals[2 * i] + romanNumerals[2 * i] + ans;
+        break;
+
+      case 3:
+        ans =
+          romanNumerals[2 * i] +
+          romanNumerals[2 * i] +
+          romanNumerals[2 * i] +
+          ans;
+        break;
+
+      case 4:
+        ans = romanNumerals[2 * i] + romanNumerals[2 * i + 1] + ans;
+        break;
+
+      case 5:
+        ans = romanNumerals[2 * i + 1] + ans;
+        break;
+
+      case 6:
+        ans = romanNumerals[2 * i + 1] + romanNumerals[2 * i] + ans;
+        break;
+
+      case 7:
+        ans =
+          romanNumerals[2 * i + 1] +
+          romanNumerals[2 * i] +
+          romanNumerals[2 * i] +
+          ans;
+        break;
+
+      case 8:
+        ans =
+          romanNumerals[2 * i + 1] +
+          romanNumerals[2 * i] +
+          romanNumerals[2 * i] +
+          romanNumerals[2 * i] +
+          ans;
+        break;
+
+      case 9:
+        ans = romanNumerals[2 * i] + romanNumerals[2 * i + 2] + ans;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return ans;
+};
+
+const startConversion = () => {
+  output.classList.remove("hidden");
+
+  const numberInt = parseInt(number.value);
+
+  if (!number.value) {
+    output.innerText = "Please enter a valid number";
+    number.value = "";
+    return;
+  } else if (numberInt < 1) {
+    output.innerText = "Please enter a number greater than or equal to 1";
+    number.value = "";
+    return;
+  } else if (numberInt > 3999) {
+    output.innerText = "Please enter a number less than or equal to 3999";
+    number.value = "";
+    return;
+  }
+
+  output.innerText = convertRomanNumeral(numberInt);
+  number.value = "";
+};
+
+convertBtn.addEventListener("click", startConversion);
+
+number.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    startConversion();
+  }
+});
